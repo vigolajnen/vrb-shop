a6696b2559ee140456e03957f6a591c613c9783b
Vigolajnen Anna <vigolajnen@gmail.com>
2024-01-14 13:16:23 +0300

    Updates

---
+++ data-dev.js
@@ -0,0 +1,42 @@
+{
+  services: [
+    {
+      city: 'Санкт-Петербург',
+      clubs: [
+        {
+          id: 44,
+          name: 'FH на Чкаловской',
+          address: 'ул. Большая Разночинная, д. 25 А',
+          isPool: true,
+          category: 'STD',
+          products: [
+            {
+              product_id: '425325',
+              product_price: '1900',
+              product_name: '1 месяц Утро СПБ Подписка',
+              product_type: 'morning',
+            },
+            {
+              product_id: '425283',
+              product_price: '2900',
+              product_name: '1 месяц СПБ Подписка',
+              product_type: 'full',
+            },
+            {
+              product_id: '425441',
+              product_price: '3500',
+              product_name: 'Мультикарта 1 месяц СПБ Подписка',
+              product_type: 'mc_full',
+            },
+            {
+              product_id: '425249',
+              product_price: '1990',
+              product_name: 'Первоначальный платеж',
+              product_type: 'first_pay',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
